ef297f4d99dbc785c5cc0349ed6b4f93a3f175e7
Karthiksgowda15 <gowdakarthiks844@gmail.com>
2025-09-14 23:39:22 +0530

    commits

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000 " ||"https://food-del-backend-eight-alpha.vercel.app";
+  const url = "http://localhost:4000 ";
 
   return (
     <div>
