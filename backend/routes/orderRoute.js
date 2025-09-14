449210f800ae8a5aed12ef48f243f2aa9b17bcb2
Karthiksgowda15 <gowdakarthiks844@gmail.com>
2025-09-14 22:54:28 +0530

    updating vercel hosted url in required files

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,13 +1,4 @@
-// import express from "express";
-// import authMiddleWare from "../middleware/auth.js";
-// import { placeOrder } from "../controllers/orderController.js";
 
-// const orderRouter = express.Router();
-
-// orderRouter.post("/place",authMiddleWare,placeOrder);
-
-
-// export default orderRouter;
 
 
 import express from "express";
